686c99c091458010077e995008680ac063aaef1c
Nelson Torres <NelsonJTSM@gmail.com>
2020-08-14 02:52:01 -0400

    Added unit tests for difficulty_rating() and hours_spent()

--- dupey-dupe/src/lib.rs
+++ dupey-dupe/src/lib.rs
@@ -1,3 +1,26 @@
-pub fn difficulty_rating() {
-    
+pub fn difficulty_rating() -> f32{
+    2.0
+}
+
+pub fn hours_spent() -> f32 {
+    3.0
+}
+
+mod tests {
+    #[allow(unused)]
+    use crate::*;
+
+    // difficulty_rating() must return a double on the range [1.0, 5.0].
+    #[test]
+    fn difficulty_rating_correct_range() {
+        let rating = difficulty_rating();
+        assert!(rating >= 1.0 && rating <= 5.0, "difficulty_rating() must return a double on the range [1.0, 5.0], instead was: {}", rating);
+    }
+
+    // hours_spent() must be strictly greater than zero.
+    #[test]
+    fn hours_spent_greater_than_one() {
+        let hours = hours_spent();
+        assert!(hours >= 0.0, "hours_spent() must be strictly greater than zero, instead was: {}", hours);
+    }
 }
